7bde428d11af75f5a47406b3baf23f1cae534c72
SrishtiAvalakki <spram001@odu.edu>
2020-02-27 12:11:59 -0500

    redux consolidation

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 //import DishDetail from './components/DishdetailComponent';
 
+const store = ConfigureStore();
+ 
 class App extends Component {
 
 
